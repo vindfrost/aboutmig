7870cd5283b84d7aa979a38ff61078870318e9ad
grubbauer <raphael.grubbauer@hotmail.com>
2025-06-03 20:05:50 +0200

    Added prestructure

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/* -------------------
+ * Location: 					src/main.cpp
+ * Purpose:						Main source file
+ * Autor:							Raphael G. Grubbauer
+ * Created:						03.06.2025
+ * Copyright:					(c) 2025 Raphael G. Grubbauer
+ * License:						MIT
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Compilation test! \n";
+	return 0;
+}
