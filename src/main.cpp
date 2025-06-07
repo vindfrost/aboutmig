f65c80b74f7d952361155e140cc0833c33d19c30
grubbauer <raphael.grubbauer@hotmail.com>
2025-06-07 12:29:56 +0200

    Updated header

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
-/* -------------------
+/* ------------------------------------------------
  * Location: 					src/main.cpp
  * Purpose:						Main source file
  * Autor:							Raphael G. Grubbauer
  * Created:						03.06.2025
  * Copyright:					(c) 2025 Raphael G. Grubbauer
  * License:						MIT
+ * ------------------------------------------------
 */
 
 #include <iostream>
