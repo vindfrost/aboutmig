38f85747b695ece861717e7f6c250359d2e85581
grubbauer <raphael.grubbauer@hotmail.com>
2025-06-08 12:00:01 +0200

    Updated documentation for the datafile

--- include/storage/datafiles.h
+++ include/storage/datafiles.h
@@ -15,9 +15,13 @@
 #include <string>
 
 namespace storage {
+// Getting the path of the datafile
 std::string getDatafile();
+// Checking if datafile exists
 bool checkForDatafile();
+// Creating datafile
 void createDatafile();
+// Saving data to the datafile
 void saveDatafile(std::string catagory, std::string value);
 }
 
