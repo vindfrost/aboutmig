ff0d5f57b402333f5b3b3fe91ecdf1130a5ff447
grubbauer <raphael.grubbauer@hotmail.com>
2025-06-09 16:06:18 +0200

    Fixed typo

--- include/storage/directory.h
+++ include/storage/directory.h
@@ -2,7 +2,7 @@
  * Location: 					src/storage/directory.h
  * Purpose:						Header file for folder
   										related storage code.
- * Autor:							Raphael G. Grubbauer
+ * Author:							Raphael G. Grubbauer
  * Created:						07.06.2025
  * Copyright:					(c) 2025 Raphael G. Grubbauer
  * License:						MIT
