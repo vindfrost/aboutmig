e6a7c79c770798e5a308b01bb52f67f7b7924bbc
grubbauer <raphael.grubbauer@hotmail.com>
2025-06-19 20:08:22 +0200

    Updated colors.

--- include/colorcodes.h
+++ include/colorcodes.h
@@ -16,24 +16,24 @@
 
 namespace colorcode{
 // Basic colors
-const std::string Black					= "\x1b[30m";
-const std::string Red						= "\x1b[31m";
-const std::string Green					= "\x1b[32m";
-const std::string Yellow				= "\x1b[33m";
-const std::string Blue					= "\x1b[34m";
-const std::string Magenta				= "\x1b[35m";
-const std::string Cyan					= "\x1b[36m";
-const std::string White					= "\x1b[37m";
+const std::string black					= "\x1b[30m";
+const std::string red						= "\x1b[31m";
+const std::string green					= "\x1b[32m";
+const std::string yellow				= "\x1b[33m";
+const std::string blue					= "\x1b[34m";
+const std::string magenta				= "\x1b[35m";
+const std::string cyan					= "\x1b[36m";
+const std::string white					= "\x1b[37m";
 
 // Bright colors
-const std::string BrightBlack   = "\x1b[90m";
-const std::string BrightRed     = "\x1b[91m";
-const std::string BrightGreen   = "\x1b[92m";
-const std::string BrightYellow  = "\x1b[93m";
-const std::string BrightBlue    = "\x1b[94m";
-const std::string BrightMagenta = "\x1b[95m";
-const std::string BrightCyan    = "\x1b[96m";
-const std::string BrightWhite   = "\x1b[97m";
+const std::string brightBlack   = "\x1b[90m";
+const std::string brightRed     = "\x1b[91m";
+const std::string brightGreen   = "\x1b[92m";
+const std::string brightYellow  = "\x1b[93m";
+const std::string brightBlue    = "\x1b[94m";
+const std::string brightMagenta = "\x1b[95m";
+const std::string brightCyan    = "\x1b[96m";
+const std::string brightWhite   = "\x1b[97m";
 
 // Reset
 const std::string Reset					= "\x1b[0m";
