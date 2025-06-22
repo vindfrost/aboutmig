0503bd46ddf058d4c6c9b9817758e25bdc155227
grubbauer <raphael.grubbauer@hotmail.com>
2025-06-22 19:34:43 +0200

    Added global logger function.

---
+++ include/log.h
@@ -0,0 +1,20 @@
+/*==============================================================================
+ * File:        log.h
+ * Path:        include/
+ * Purpose:     Logger header for global logging
+ * 
+ * Author:      Raphael G. Grubbauer
+ * Created:     2025-06-22
+ * License:     MIT
+ * Copyright:   (c) 2025 Raphael G. Grubbauer
+ *============================================================================*/
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "spdlog/logger.h"
+
+std::string getLogDate();
+std::shared_ptr<spdlog::logger> get_logger();
